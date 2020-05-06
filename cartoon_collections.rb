63edbb6e1ccd2c359d20ab044332d4d7c3067536
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 13:35:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,16 +18,21 @@ def summon_captain_planet(array)# code an argument here
   summoned_planet
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  newarray = []
   i = 0
   while i < array.length
-    if condition
-
+    newarray << array[i]
+    i += 1
+    if i > 3
+      true
+      
     else
-
+      false
     end
   end
+  
 end
 
 def find_the_cheese# code an argument here
