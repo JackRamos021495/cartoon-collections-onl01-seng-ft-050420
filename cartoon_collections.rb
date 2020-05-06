aca41d99a895f10e9ea1d2d7526a6b397d0ef945
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 13:35:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,12 +27,12 @@ def long_planeteer_calls(array)# code an argument here
     i += 1
     if i > 3
       true
-      
+
     else
       false
     end
   end
-  
+
 end
 
 def find_the_cheese# code an argument here
