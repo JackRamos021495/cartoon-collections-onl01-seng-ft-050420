f6bcabf2a74f10d635c55b40c06da32e7377afc9
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 13:13:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,11 +5,16 @@ def roll_call_dwarves(array)# code an argument here
     puts "#{i+1}. #{array[i]}"
     i += 1
   end
-
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  i = 0
+  array.split("!")
+  array.collect do |phrase|
+    phrase.capitalize
+  end
+  array.join(" ")
 end
 
 def long_planeteer_calls# code an argument here
