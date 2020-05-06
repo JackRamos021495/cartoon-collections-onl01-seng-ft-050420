8aed02d240e0acb2cc36de18cfc99593e9db1c3f
Jackson Ramos <jackramos021495@gmail.com>
2020-05-06 13:49:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.find{|i| i.is_a?(cheddar)}
-    return i
+    return array[i]
 
   else
     nil
